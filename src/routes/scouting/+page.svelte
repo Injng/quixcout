65bdfc290a06f7b2a4a00a917aeab1a7f837aa8e
Lin Jiang <jianglin@lnjng.com>
2025-02-13 15:16:11 -0800

    feat: add components for scouting form tabs

--- src/routes/scouting/+page.svelte
+++ src/routes/scouting/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import * as Select from "$lib/components/ui/select/index.js";
+  import * as Tabs from "$lib/components/ui/tabs/index.js";
   import { buttonVariants } from "$lib/components/ui/button/index.js";
   import EventForm from "./components/EventForm.svelte";
   import type { PageData } from "./$types.js";
   import { goto } from "$app/navigation";
   import DataTable from "./components/DataTable.svelte";
   import { columns } from "./schema/columns";
+  import MetadataForm from "./components/MetadataForm.svelte";
+  import AutonForm from "./components/AutonForm.svelte";
 
   let { data }: { data: PageData } = $props();
 
@@ -16,7 +19,6 @@
   // watch for changes to the selected event and set url
   $effect(() => {
     if (selectedEvent) {
-      console.log(selectedEvent);
       goto(`?event=${selectedEvent}`);
     }
   });
@@ -24,6 +26,9 @@
   // state to track what the page is showing
   // 0 = data table, 1 = scouting forms
   let pageState = $state(0);
+  $effect(() => {
+    console.log(pageState);
+  });
 </script>
 
 <div>
@@ -76,7 +81,7 @@
             >
             <EventForm
               data={{
-                form: data.form ?? {
+                form: data.eventForm ?? {
                   id: "",
                   valid: false,
                   posted: false,
@@ -93,6 +98,24 @@
     <!-- Data Table -->
     <div class="mt-2">
       <DataTable data={data.team_data ?? []} {columns} />
+    </div>
+  {:else if pageState == 1}
+    <!-- Scouting Form Tabs -->
+    <div class="flex justify-center mt-2 mb-2">
+      <Tabs.Root value="team" class="w-[400px]">
+        <Tabs.List class="grid w-full grid-cols-4">
+          <Tabs.Trigger value="team">Team</Tabs.Trigger>
+          <Tabs.Trigger value="auton">Auton</Tabs.Trigger>
+          <Tabs.Trigger value="teleop">TeleOp</Tabs.Trigger>
+          <Tabs.Trigger value="endgame">Endgame</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="team">
+          <MetadataForm data={{ form: data.metadataForm }} />
+        </Tabs.Content>
+        <Tabs.Content value="auton">
+          <AutonForm data={{ form: data.autonForm }} />
+        </Tabs.Content>
+      </Tabs.Root>
     </div>
   {/if}
 </div>
