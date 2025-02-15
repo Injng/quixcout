069f0593ab9e254f202569f1027c3063bed2f885
Lin Jiang <jianglin@lnjng.com>
2025-02-15 10:16:12 -0800

    fix: login form boxes for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-  import type { PageData } from "./$types.js";
-  import LoginForm from "./LoginForm.svelte";
-  import { ModeWatcher } from "mode-watcher";
-  
-  let { data }: { data: PageData } = $props();
+    import type { PageData } from "./$types.js";
+    import LoginForm from "./LoginForm.svelte";
+    import { ModeWatcher } from "mode-watcher";
+
+    let { data }: { data: PageData } = $props();
 </script>
 
 <div>
-  <!-- Header -->
-  <div class="bg-slate-500 flex flex-row font-serif p-5 z-10">
-    <a href="/" class="font-bold">QUIXCOUT</a>
-  </div>
-  <div class="m-52">
-    <LoginForm {data} />
-  </div>
+    <!-- Header -->
+    <div class="bg-black/60 text-white flex flex-row font-serif p-5 z-10 m-5">
+        <div class="font-bold grow text-center">QUIXCOUT</div>
+    </div>
+    <div class="mx-4 md:mx-52 my-8">
+        <LoginForm {data} />
+    </div>
 </div>
-
