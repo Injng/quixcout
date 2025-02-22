fff2017504870a495198964a12d8d79880ca0e3a
Lin Jiang <jianglin@lnjng.com>
2025-02-22 11:34:45 -0800

    feat: add larger increment buttons for better accessibility

--- src/routes/scouting/components/AutonForm.svelte
+++ src/routes/scouting/components/AutonForm.svelte
@@ -1,157 +1,167 @@
 <script lang="ts">
-  import * as Form from "$lib/components/ui/form/index.js";
-  import { Checkbox } from "$lib/components/ui/checkbox/index.js";
-  import { Input } from "$lib/components/ui/input/index.js";
-  import { type Infer } from "sveltekit-superforms";
-  import { type ScoutingSchema } from "../schema/schema";
-  import type { FsSuperForm } from "formsnap";
-  import type { Writable } from "svelte/store";
+    import * as Form from "$lib/components/ui/form/index.js";
+    import { Checkbox } from "$lib/components/ui/checkbox/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { type Infer } from "sveltekit-superforms";
+    import { type ScoutingSchema } from "../schema/schema";
+    import type { FsSuperForm } from "formsnap";
+    import type { Writable } from "svelte/store";
 
-  export let form: FsSuperForm<Infer<ScoutingSchema>>;
-  export let formData: Writable<Infer<ScoutingSchema>>;
+    export let form: FsSuperForm<Infer<ScoutingSchema>>;
+    export let formData: Writable<Infer<ScoutingSchema>>;
 </script>
 
 <Form.Field {form} name="auton_parking">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Auton Parking</Form.Label>
-      <Checkbox {...props} bind:checked={$formData.auton_parking} />
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Auton Parking</Form.Label>
+            <Checkbox {...props} bind:checked={$formData.auton_parking} />
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="auton_push_samples">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Auton Push Samples</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.auton_push_samples--}
-          class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.auton_push_samples}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.auton_push_samples++}
-          class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Auton Push Samples</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_push_samples--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.auton_push_samples}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_push_samples++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="auton_low_basket_samples">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Auton Low Basket Samples</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.auton_low_basket_samples--}
-          class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.auton_low_basket_samples}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.auton_low_basket_samples++}
-          class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Auton Low Basket Samples</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_low_basket_samples--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.auton_low_basket_samples}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_low_basket_samples++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="auton_high_basket_samples">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Auton High Basket Samples</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.auton_high_basket_samples--}
-          class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.auton_high_basket_samples}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.auton_high_basket_samples++}
-          class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Auton High Basket Samples</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_high_basket_samples--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.auton_high_basket_samples}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_high_basket_samples++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="auton_low_chamber_specimen">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Auton Low Chamber Specimen</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.auton_low_chamber_specimen--}
-          class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.auton_low_chamber_specimen}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.auton_low_chamber_specimen++}
-          class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Auton Low Chamber Specimen</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_low_chamber_specimen--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.auton_low_chamber_specimen}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_low_chamber_specimen++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="auton_high_chamber_specimen">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Auton High Chamber Specimen</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.auton_high_chamber_specimen--}
-          class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.auton_high_chamber_specimen}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.auton_high_chamber_specimen++}
-          class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Auton High Chamber Specimen</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_high_chamber_specimen--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.auton_high_chamber_specimen}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.auton_high_chamber_specimen++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
--- src/routes/scouting/components/TeleOpForm.svelte
+++ src/routes/scouting/components/TeleOpForm.svelte
@@ -1,134 +1,156 @@
 <script lang="ts">
-  import * as Form from "$lib/components/ui/form/index.js";
-  import { Input } from "$lib/components/ui/input/index.js";
-  import { type Infer } from "sveltekit-superforms";
-  import { type ScoutingSchema } from "../schema/schema";
-  import type { FsSuperForm } from "formsnap";
-  import type { Writable } from "svelte/store";
+    import * as Form from "$lib/components/ui/form/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { type Infer } from "sveltekit-superforms";
+    import { type ScoutingSchema } from "../schema/schema";
+    import type { FsSuperForm } from "formsnap";
+    import type { Writable } from "svelte/store";
 
-  export let form: FsSuperForm<Infer<ScoutingSchema>>;
-  export let formData: Writable<Infer<ScoutingSchema>>;
+    export let form: FsSuperForm<Infer<ScoutingSchema>>;
+    export let formData: Writable<Infer<ScoutingSchema>>;
 </script>
 
 <Form.Field {form} name="total_push_samples">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Total Push Samples</Form.Label>
-      <div class="flex items-center">
-        <button type="button" onclick={() => $formData.total_push_samples--} class="m-2"
-          >-</button
-        >
-        <Input
-          {...props}
-          type="number"
-          bind:value={$formData.total_push_samples}
-          class="text-center w-12 mx-2"
-        />
-        <button type="button" onclick={() => $formData.total_push_samples++} class="m-2"
-          >+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Total Push Samples</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.total_push_samples--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    type="number"
+                    bind:value={$formData.total_push_samples}
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.total_push_samples++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="total_low_basket_samples">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Total Low Basket Samples</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.total_low_basket_samples--} class="m-2">-</button
-        >
-        <Input
-          {...props}
-          type="number"
-          bind:value={$formData.total_low_basket_samples}
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.total_low_basket_samples++} class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Total Low Basket Samples</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.total_low_basket_samples--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    type="number"
+                    bind:value={$formData.total_low_basket_samples}
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.total_low_basket_samples++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="total_high_basket_samples">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Total High Basket Samples</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.total_high_basket_samples--} class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.total_high_basket_samples}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.total_high_basket_samples++} class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Total High Basket Samples</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.total_high_basket_samples--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.total_high_basket_samples}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.total_high_basket_samples++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="total_low_chamber_specimen">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Total Low Chamber Specimen</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.total_low_chamber_specimen--} class="m-2">-</button
-        >
-        <Input
-          {...props}
-          type="number"
-          bind:value={$formData.total_low_chamber_specimen}
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.total_low_chamber_specimen++} class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Total Low Chamber Specimen</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.total_low_chamber_specimen--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    type="number"
+                    bind:value={$formData.total_low_chamber_specimen}
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.total_low_chamber_specimen++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
 
 <Form.Field {form} name="total_high_chamber_specimen">
-  <Form.Control>
-    {#snippet children({ props })}
-      <Form.Label>Total High Chamber Specimen</Form.Label>
-      <div class="flex items-center">
-        <button
-          type="button"
-          onclick={() => $formData.total_high_chamber_specimen--} class="m-2">-</button
-        >
-        <Input
-          {...props}
-          bind:value={$formData.total_high_chamber_specimen}
-          type="number"
-          class="text-center w-12 mx-2"
-        />
-        <button
-          type="button"
-          onclick={() => $formData.total_high_chamber_specimen++} class="m-2">+</button
-        >
-      </div>
-    {/snippet}
-  </Form.Control>
-  <Form.FieldErrors />
+    <Form.Control>
+        {#snippet children({ props })}
+            <Form.Label>Total High Chamber Specimen</Form.Label>
+            <div class="flex items-center">
+                <button
+                    type="button"
+                    onclick={() => $formData.total_high_chamber_specimen--}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&minus;</button
+                >
+                <Input
+                    {...props}
+                    bind:value={$formData.total_high_chamber_specimen}
+                    type="number"
+                    class="text-center w-12 mx-2"
+                />
+                <button
+                    type="button"
+                    onclick={() => $formData.total_high_chamber_specimen++}
+                    class="w-10 h-10 text-2xl rounded-lg m-4 outline outline-2"
+                    >&plus;</button
+                >
+            </div>
+        {/snippet}
+    </Form.Control>
+    <Form.FieldErrors />
 </Form.Field>
